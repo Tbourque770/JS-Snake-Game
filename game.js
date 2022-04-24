d87a23e9a8ac0137487ad0535c9cbc40a4fe6983
Tyler Bourque <tyler@Tylers-PC>
2022-04-23 19:42:09 -0600

    Primary Files

---
+++ game.js
@@ -0,0 +1,46 @@
+import {update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersect} from './snake.js'
+import {update as updateFood, draw as drawFood} from './food.js'
+import {outsideGrid} from './grid.js'
+
+let lastRenderTime = 0
+const gameBoard = document.getElementById('game-board')
+let gameOver = false
+
+function main(currentTime) {
+
+    if (gameOver) {
+        if (confirm("You lost. Press ok to play again.")) {
+            window.location = '/'
+        }
+        return
+    }
+
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+    updateFood()
+    checkDeath()
+}
+
+function draw() {
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() { 
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersect()
+}
